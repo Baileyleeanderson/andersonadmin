155f9ed03490b7b4ac349930448f8d34c0962045
Ubuntu <ubuntu@ip-172-31-16-110.ap-southeast-1.compute.internal>
2018-12-21 15:55:03 +0000

    fixed andersonadmin.com bug

--- andersonadmin/settings.py
+++ andersonadmin/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y_y$n^9zziy%^7w$p=vlqedo5+9ms%wozaax!hb-1-z6)wmlq@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.221.220.20', '.andersonadmin.com']
 
 # Application definition
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
